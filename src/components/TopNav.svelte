807c32d8cd44593cf8cf919c37512fa0692af46d
Kuririn <kuririn1@gmail.com>
2022-12-22 15:33:42 +0100

    added spinner for the profile load state

--- src/components/TopNav.svelte
+++ src/components/TopNav.svelte
@@ -7,13 +7,16 @@
     const deso = $desoApi;
     let profilePic = '';
     let username = '...';
+    let isLoadingProfile = false;
 
     async function login() {
         const response = await deso.identity.login('4');
         if(response?.key?.length > 0) {
             $isUserLogged = true;
+            isLoadingProfile = true;
             profilePic = await getProfilePic();
             username = (await getProfile()).Profile.Username;
+            isLoadingProfile = false;
         }
     }
 
@@ -39,8 +42,10 @@
 
     onMount(async () => {
         if($isUserLogged) {
+            isLoadingProfile = true;
             profilePic = await getProfilePic();
             username = (await getProfile()).Profile.Username;
+            isLoadingProfile = false;
         }
     });
 
@@ -53,7 +58,7 @@
             <img src={logo} class="h-9" alt="SurfSwap">
         </div>
         <div class="flex-none">
-            {#if $isUserLogged}
+            {#if $isUserLogged && !isLoadingProfile}
                 <div class="flex flex-row">
                     <div class="bg-slate-100 rounded-lg p-1">
                         <div class="flex flex-row">
@@ -69,10 +74,15 @@
                         <img on:click={logout} alt="logout" src={logoutIcon} class="mt-2 h-5 ring-4 rounded bg-gray-100 ring-gray-100 hover:cursor-pointer" />
                     </div>
                 </div>
+            {:else if isLoadingProfile}
+                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="#6366f1" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="#6366f1" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
             {:else}
                 <button on:click={login} class="bg-gradient-to-r from-indigo-500 to-indigo-400 hover:shadow-[0_0_22px_-7px_rgba(1,4,232,0.8)] rounded-lg text-white text-sm py-2 px-4">
-                    Login with Deso
-                </button>
+                        Login with Deso
+                </button>   
             {/if}
         </div>
     </div>    
